5491874f574943add42d51cfabd8e67794afb414
Bilguun Gantogos <gantogos.bilguun48@gmail.com>
2024-03-28 11:27:54 +0800

    themetoggle ustgav

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -1,43 +1,5 @@
 "use client";
 
-import { Moon, Sun } from "lucide-react";
-import { useTheme } from "next-themes";
-
 export function ThemeToggle() {
-  const { setTheme, theme } = useTheme();
-
-  return (
-    <div>
-      <button
-        onMouseDown={() => setTheme(theme === "light" ? "dark" : "light")}
-        className="relative"
-      >
-        {theme === "light" ? (
-          <>
-            <Sun
-              size="80"
-              className="absolute transition-all scale-100 rotate-0 text-slate-100"
-            />
-            <Moon
-              size="80"
-              className="absolute transition-all scale-0 rotate-90"
-              style={{ visibility: "hidden" }}
-            />
-          </>
-        ) : (
-          <>
-            <Sun
-              size="80"
-              className="absolute transition-all scale-0 rotate-90"
-              style={{ visibility: "hidden" }}
-            />
-            <Moon
-              size="80"
-              className="absolute transition-all scale-100 rotate-0 text-slate-100"
-            />
-          </>
-        )}
-      </button>
-    </div>
-  );
+  return <div></div>;
 }
