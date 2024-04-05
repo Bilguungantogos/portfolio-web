20e211745873daf1a6b831ad9ff4c765e34e74ec
Билгүүн Гантогос <LP230023@G301-CC0190.local>
2024-04-05 01:56:43 -0700

    header titles

--- components/header.tsx
+++ components/header.tsx
@@ -9,6 +9,12 @@ const Header = () => {
   const { theme } = useTheme();
   const { showAllGrid, showAbout, showMedia, showProject } =
     useContext(LayoutContext);
+  const [headerTitles, setHeaderTitles] = useState([
+    { name: "All", isActive: true },
+    { name: "About", isActive: false },
+    { name: "Project", isActive: false },
+    { name: "Media", isActive: false },
+  ]);
   return (
     <div className="flex justify-around items-center max-md:flex-col max-md:gap-4 py-4 mt-4 dark:text-white text-black max-w-[1250px] mx-auto">
       {theme === "light" ? (
@@ -27,28 +33,28 @@ const Header = () => {
 
       <div className="flex items-center justify-center lg:gap-8 h-10 border-2 border-[#31363d] border-opacity-50 rounded-full px-2 lg:text-lg md:text-md sm:text-sm xs:text-xs shadow-2xl">
         <button
-          className="hover:bg-slate-300 rounded-full px-3 "
+          className={`hover:bg-slate-300 rounded-full px-3 `}
           onClick={showAllGrid}
         >
-          All
+          {headerTitles[0].name}
         </button>
         <button
           className="hover:bg-slate-300 rounded-full px-2"
           onClick={showAbout}
         >
-          About
+          {headerTitles[1].name}
         </button>
         <button
           className="hover:bg-slate-300 rounded-full px-2"
           onClick={showProject}
         >
-          Project
+          {headerTitles[2].name}
         </button>
         <button
           className="hover:bg-slate-300 rounded-full px-2"
           onClick={showMedia}
         >
-          Media
+          {headerTitles[3].name}
         </button>
       </div>
       <motion.button
