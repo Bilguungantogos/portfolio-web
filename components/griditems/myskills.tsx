ccb90e3cad31fdd9244b966aa79e2ae7a166edec
Bilguun Gantogos <gantogos.bilguun48@gmail.com>
2024-04-06 13:15:38 +0800

    cinema added

--- components/griditems/myskills.tsx
+++ components/griditems/myskills.tsx
@@ -11,32 +11,6 @@ import { BiLogoPostgresql } from "react-icons/bi";
 import { IoLogoSass } from "react-icons/io";
 import { motion } from "framer-motion";
 
-const MySkills = () => {
-  return (
-    <div className="flex flex-col justify-between select-none ">
-      <p className="text-light text-lg text-slate-600 px-6 max-[481px]:px-4 max-[481px]:text-sm select-none dark:text-slate-200">
-        I have been working on a project involving the utilization of these
-        languages and tools:
-      </p>
-      <p className="flex flex-wrap justify-center gap-8 max-[481px]:gap-4">
-        {skillIcons.map((e) => {
-          return (
-            <motion.button
-              whileHover={{ scale: 1.2 }}
-              whileTap={{ scale: 0.9 }}
-              className="size-[40px] max-[481px]:size-[30px]"
-            >
-              <a href={e.href}>{e.icon}</a>
-            </motion.button>
-          );
-        })}
-      </p>
-    </div>
-  );
-};
-
-export default MySkills;
-
 const skillIcons = [
   {
     icon: <IoLogoJavascript size={"full"} />,
@@ -79,3 +53,29 @@ const skillIcons = [
     href: "https://sass-lang.com",
   },
 ];
+
+const MySkills = () => {
+  return (
+    <div className="flex flex-col justify-between select-none ">
+      <p className="text-light text-lg text-slate-600 px-6 max-[481px]:px-4 max-[481px]:text-sm select-none dark:text-slate-200">
+        I have been working on a project involving the utilization of these
+        languages and tools:
+      </p>
+      <p className="flex flex-wrap justify-center gap-8 max-[481px]:gap-4">
+        {skillIcons.map((e) => {
+          return (
+            <motion.button
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 0.9 }}
+              className="size-[40px] max-[481px]:size-[30px]"
+            >
+              <a href={e.href}>{e.icon}</a>
+            </motion.button>
+          );
+        })}
+      </p>
+    </div>
+  );
+};
+
+export default MySkills;
